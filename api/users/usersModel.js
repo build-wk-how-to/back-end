9d5a81e12b52ec24295c3c251bef1a3a1158a67b
spinon187 <spinon187@gmail.com>
2019-07-10 12:47:44 -0500

    i hate knex even more 4

--- api/users/usersModel.js
+++ api/users/usersModel.js
@@ -14,11 +14,11 @@ module.exports = {
 };
 
 function insertUser(user) {
-  // return db('users')
-  //   .insert(user)
+  return db('users')
+    .insert(user)
+  // //   .then(ids => ({ userID: ids[0], username: user.username, password: user.password, usertype: user.usertype }));
+  // db.insert(user).into('users')
   //   .then(ids => ({ userID: ids[0], username: user.username, password: user.password, usertype: user.usertype }));
-  db.insert(user).into('users')
-    .then(ids => ({ userID: ids[0], username: user.username, password: user.password, usertype: user.usertype }));
 }
 
 function insertGuide(guide){
